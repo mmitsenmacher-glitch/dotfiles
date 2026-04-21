214ea451fa6d6bdd73d323a519af29372892e5a6
Michael Mitsenamcher <mmitsenmacher@coreweave.com>
2026-04-21 10:09:07 -0400

    Add git/vim fix setup for zsh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
-# Install zsh
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+if [ -n "$BASH_SOURCE" ]; then
+    # We are in Bash
+    RELATIVE_PATH="${BASH_SOURCE[0]}"
+elif [ -n "$ZSH_VERSION" ]; then
+    # We are in Zsh
+    RELATIVE_PATH="$0"
+else
+    # Fallback for other shells
+    RELATIVE_PATH="$0"
+fi
+
+# 2. Resolve the absolute directory
+SCRIPT_DIR=$(cd -- "$(dirname -- "$RELATIVE_PATH")" &> /dev/null && pwd)
+
 
 ln -sf $SCRIPT_DIR/../bash/.bashrc-${USER}    ~/.bashrc-${USER}
 ln -sf $SCRIPT_DIR/../zsh/.zshrc-${USER}      ~/.zshrc-${USER}
 ln -sf $SCRIPT_DIR/../tmux/.tmux.conf ~/.tmux.conf
-
+ln -sf $SCRIPT_DIR/../vim/.vimrc ~/.vimrc
+ln -sf $SCRIPT_DIR/../git/.gitconfig  ~/.gitconfig
 # Install zsh-history-substring-search
